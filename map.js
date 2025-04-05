dbd395eed8df9eb1de4e730a951489fe1fb9e702
Jasuv <ejahsue@gmai.com>
2025-04-04 18:08:54 -0700

    map stuff

---
+++ map.js
@@ -0,0 +1,21 @@
+// create map
+var map = L.map('map').setView([51.505, -0.09], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+// marker demo
+var marker = L.marker([51.5, -0.09]).addTo(map);
+// popup demo
+marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+
+// interaction with popup demo
+var popup = L.popup();
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+map.on('click', onMapClick);
